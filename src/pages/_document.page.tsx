a96fcaedd47c9ffdda4eab70984d70d02de1efc7
Evandro Marinho <evanforgotlastone@gmail.com>
2025-01-15 00:31:29 -0300

    feat: pageExtensions

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -9,13 +9,13 @@ class MyDocument extends Document {
           {/* Link to Google Fonts for Roboto */}
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
 
-          <style id='stiches' dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         </Head>
         <body>
           <Main />
