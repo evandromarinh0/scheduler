557a8b35ff46fd90891c22a0694e349965b7e873
Evandro Marinho <evanforgotlastone@gmail.com>
2025-01-19 19:45:35 -0300

    feat: react hook form and zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,16 +1,47 @@
-import { Button, TextInput } from "@ignite-ui/react";
-import { Form } from "./styles";
-import { ArrowRight } from "phosphor-react";
+import { Button, Text, TextInput } from "@ignite-ui/react"
+import { Form, FormError } from "./styles"
+import { ArrowRight } from "phosphor-react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const usernameSchema = z.object({
+  username: z
+  .string()
+  .min(3, { message: 'Digite um nome de usuário' })
+  .regex(/^([a-z\\\\-]+)$/i, { message: 'Usuário pode conter apenas letras e hifen' })
+  .transform(username => username.toLocaleLowerCase())
+})
+
+type usernameFormData = z.infer<typeof usernameSchema>
 
 
 export function ClaimUsernameForm(){
+  const { register, handleSubmit, formState: { errors } } = useForm<usernameFormData>({
+    resolver: zodResolver(usernameSchema)
+  })
+  
+  async function handleRegister(data: usernameFormData){
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="call.er/" placeholder="seu-usuário" />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
+        <TextInput size="sm" prefix="call.er/" placeholder="seu-usuário" {...register('username')} />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+
+        
+
+      </Form>
+      <FormError>
+        <Text size="sm">
+          {errors.username ? errors.username.message : 'Digite o nome de usuário desejado'}
+        </Text>
+      </FormError>
+    </>
   )
 }
